bbaa7dfa879a97f67a58cb4b5432784915dde8fc
Quentin GLX <quentin.glorieux@lkb.upmc.fr>
2023-02-14 14:24:39 +0100

    update AppConfig.vue and NavSource.vue

--- components/app/AppConfig.vue
+++ components/app/AppConfig.vue
@@ -1,22 +1,28 @@
 <script setup>
-
 const visible = ref(false);
 
 const onConfigButtonClick = () => {
-    visible.value = !visible.value;
+  visible.value = !visible.value;
 };
-
 </script>
 
 <template>
-    <button class="layout-config-button p-link" type="button" @click="onConfigButtonClick()">
-        <i class="pi pi-chevron-circle-left"></i>
-    </button>
+  <button
+    class="layout-config-button p-link"
+    type="button"
+    @click="onConfigButtonClick()"
+  >
+    <i class="pi pi-chevron-circle-left"></i>
+  </button>
 
-    <Sidebar v-model:visible="visible" position="right" :transitionOptions="'.3s cubic-bezier(0, 0, 0.2, 1)'" class="layout-config-sidebar w-20rem">
-      Menu de droite
-    </Sidebar>
-    
+  <Sidebar
+    v-model:visible="visible"
+    position="right"
+    :transitionOptions="'.3s cubic-bezier(0, 0, 0.2, 1)'"
+    class="layout-config-sidebar w-20rem"
+  >
+    Menu de droite
+  </Sidebar>
 </template>
 
 <style lang="scss" scoped></style>
--- components/sources/NavSource.vue
+++ components/sources/NavSource.vue
@@ -1,90 +1,73 @@
 <template>
-    <div class="nav-source pl-2"> 
-      
+  <div class="nav-source pl-2">
     <i class="pi pi-fw pi-file"></i>
-    <span class="py-1 text-xl">  SOURCES  </span>
-    
-    <div style="margin-bottom: 1em">
-            <Button type="button" icon="pi pi-plus" label="Expand All" @click="expandAll" />
-            <Button type="button" icon="pi pi-minus" label="Collapse All" @click="collapseAll" />
-        </div>
-    <Tree :value="nodes" :filter="true" filterMode="lenient"></Tree>
-    </div>  
-</template>
+    <span class="py-1 text-xl"> SOURCES </span>
 
-<style scoped>
-.nav-source{
-    height: calc(100vh - 5rem);
-    width: 300px;
-}
-.p-button {
-    margin-right: .5rem;
-}
-</style>
+    <div style="margin-bottom: 1em">
+      <Button
+        type="button"
+        icon="pi pi-plus"
+        label="Expand All"
+        @click="expandAll()"
+      />
+      <Button
+        type="button"
+        icon="pi pi-minus"
+        label="Collapse All"
+        @click="collapseAll()"
+      >
+      </Button>
+    </div>
+    <Tree :value="nodes" v-model:expandedKeys="expandedKeys" :filter="true" filterMode="lenient"></Tree>
+  </div>
+</template>
 
 <script setup>
 // import NodeService from '@/utils/NodeService';
-import dataJson from '@/assets/data/treenodes.json'
-
+import dataJson from "@/assets/data/treenodes.json";
 
 onMounted(() => {
-            // nodeService.value.getTreeNodes().then(data => nodes.value = data);
-            nodes.value = dataJson.root;
-        })
-
-        const nodes = ref(null);
-        // const nodeService = ref(new NodeService());
-        const expandedKeys = ref({});
-        const expandAll = () => {
-            for (let node of nodes.value) {
-                expandNode(node);
-            }
-
-            expandedKeys.value = {...expandedKeys.value};
-        };
-        const collapseAll = () => {
-            expandedKeys.value = {};
-        };
-        const expandNode = (node) => {
-            expandedKeys.value[node.key] = true;
-            if (node.children && node.children.length) {
-                for (let child of node.children) {
-                    expandNode(child);
-                }
-            }
-        };
+  // nodeService.value.getTreeNodes().then(data => nodes.value = data);
+  nodes.value = dataJson.root;
+});
 
 
-const model = ref([
-    {
-        label: 'Article1',
-        items: [
-            { label: 'Section1', to: '/' },
-            { label: 'Section2', 
-            items: [
-                { label: 'Clause 1', to: '/clause1'},
-                { label: 'Clause 2', to: '/clause2'},
-                { label: 'Clause 3', to: '/clause3'}
-                        ] },
-        ]
-    },
-    {
-        label: 'Article2',
-        items: [
-            { label: 'Section1', 
-            items: [
-                { label: 'Clause 1', to: '/clause1'},
-                { label: 'Clause 2 et 3', to: '/clause2'}
-            ],
-            label: 'Section2', 
-            items: [
-                { label: 'Clause 1', to: '/clause1'},
-                { label: 'Clause 2 et 3', to: '/clause2'}
-                        ] },
-        ]
-    },
 
-])
+const nodes = ref(null);
+// const nodeService = ref(new NodeService());
+const expandedKeys = ref({});
+
+const expandAll = () => {
+  for (let node of nodes.value) {
+    expandNode(node);
+  }
+  expandedKeys.value = { ...expandedKeys.value };
+  console.log(expandedKeys.value)
+};
+
+const collapseAll = () => {
+  expandedKeys.value = {};
+};
+
+const expandNode = (node) => {
+  expandedKeys.value[node.key] = true;
+  if (node.children && node.children.length) {
+    for (let child of node.children) {
+      expandNode(child);
+    }
+  }
+};
 
 
 </script>
+
+<style scoped>
+.nav-source {
+  height: calc(100vh - 5rem);
+  width: 300px;
+}
+.p-button {
+  margin-right: 0.5rem;
+}
+</style>
+
