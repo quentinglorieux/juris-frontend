1c2ad0d2515a7d8514e9cb76416c3e298c95a932
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-02-15 00:56:21 +0100

    update AppLayout.vue, default.vue and nuxt.config.ts

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,7 +63,7 @@ const isOutsideClicked = (event) => {
             </div>
             <app-footer></app-footer>
         </div>
-        <app-config></app-config>
+        <!-- <app-config></app-config> -->
         <div class="layout-mask"></div>
     </div>
 </template>
