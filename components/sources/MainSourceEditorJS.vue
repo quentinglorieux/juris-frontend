2c6b126e860ec0ba29cff4db85acbe0e36370c79
olivierglrx <olivier.glrx@gmail.com>
2023-06-02 12:05:28 +0200

    correction bug second.data

--- components/sources/MainSourceEditorJS.vue
+++ components/sources/MainSourceEditorJS.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>  
+  <div style="width:100px">  
     <div v-for="item in listItems">
       <div v-for="block in item">
         <div v-html="block"></div>
@@ -11,21 +11,22 @@
 <script setup>
 import { Directus } from "@directus/sdk";
 import edjsHTML from "editorjs-html";
-const directus = new Directus("http://127.0.0.1:8055/");
+// const directus = new Directus("http://127.0.0.1:8055/");
+const directus = new Directus("https://jurisdirectus.rubidiumweb.eu/");
+
 const edjsParser = edjsHTML({ nestedlist: nestedListParser });
 function nestedListParser(block) {
   const data = block.data;
-  console.log(data);
   const type = data.style === "ordered" ? "ol" : "ul";
-  console.log(data.style);
-  console.log(type);
   const items = data.items.reduce((acc, item) => acc + `<li>${item.content}</li>`, "");
   return `<${type}>${items}</${type}>`;
 }
 const listItems = ref([]);
 async function retrieveSources() {
-  const publicData = await directus.items("Source").readByQuery({ fields: ["editorjs"] });
+  const publicData = await directus.items("sources").readByQuery();
+  
   var L = publicData.data;
+  console.log(L[0])
   listItems.value = L.map((i) => edjsParser.parse(i.editorjs));
 }
 retrieveSources();
