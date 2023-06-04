019647ef3dd413912984e85abcf884b234f767a2
olivierglrx <olivier.glrx@gmail.com>
2023-06-04 17:08:25 +0200

    editorJS

--- components/sources/MainSource.vue
+++ components/sources/MainSource.vue
@@ -67,7 +67,7 @@ const emit = defineEmits(["comIsSelected"]);
 import edjsHTML from "editorjs-html";
 const edjsParser = edjsHTML();
 const editorJScontent = computed(() => {
-  // return edjsParser.parse(props.source.data.EditorJS);
+  return edjsParser.parse(props.source.data.EditorJS);
 });
 
 onUpdated(() => {
